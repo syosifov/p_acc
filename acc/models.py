e8904f8d2e487aee1191d14b708a0c4103a5c885
Strashimir Yosifov <syosifov@gmail.com>
2022-06-09 09:27:32 +0300

    acc_type defined

--- acc/models.py
+++ acc/models.py
@@ -1,14 +1,19 @@
+import imp
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
+ACC_A = 0
+ACC_P = 1
+ACC_AP = 2
 
 class Account(models.Model):
-    name = models.CharField(max_length=20, null=False,blank=False,unique=True, primary_key=True)
+    name = models.CharField(max_length=20, null=False,blank=False,unique=True, primary_key=True, default=' ')
     description = models.CharField(max_length=20, null=False,blank=False,unique=True, default='Balance')
     assets = models.DecimalField(max_digits=10, decimal_places=4, default=0)
     liabilities = models.DecimalField(
         max_digits=10, decimal_places=4, default=0)
+    acc_type = models.IntegerField(default=ACC_A)
     balance = models.DecimalField(max_digits=10, decimal_places=4, default=0)
 
     parent = models.ForeignKey('self', 
@@ -19,3 +24,7 @@ class Account(models.Model):
     
     def debit(self, n):
         pass
+
+    def __str__(self) -> str:
+        return self.name
+    
