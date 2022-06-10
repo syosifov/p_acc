a45741a152241e890369393aeb53ecc2b7d21ddf
Strashimir Yosifov <syosifov@gmail.com>
2022-06-10 19:42:05 +0300

    Accounts generated

--- acc/models.py
+++ acc/models.py
@@ -8,12 +8,12 @@ ACC_P = 1
 ACC_AP = 2
 
 class Account(models.Model):
-    name = models.CharField(max_length=20, null=False,blank=False,unique=True, primary_key=True, default=' ')
+    name = models.CharField(max_length=20, null=False,blank=False,unique=True, primary_key=True, default='0')
     description = models.CharField(max_length=100, null=False,blank=False,unique=True, default='Balance')
     assets = models.DecimalField(max_digits=10, decimal_places=4, default=0)
     liabilities = models.DecimalField(
         max_digits=10, decimal_places=4, default=0)
-    acc_type = models.IntegerField(default=ACC_A)
+    acc_type = models.IntegerField(default=ACC_AP)
     balance = models.DecimalField(max_digits=10, decimal_places=4, default=0)
 
     parent = models.ForeignKey('self', 
--- acc/views.py
+++ acc/views.py
@@ -12,6 +12,7 @@ def save_acc(acc):
     serializer = AccountSerializer(data=acc)
     if serializer.is_valid():
         serializer.save()
+        print(serializer.data)
         return serializer.data
     else:
         raise Exception(serializer.error_messages)
@@ -25,23 +26,32 @@ def db_init(request):
     
     rout = save_acc({})
     section = {}
-    section['name'] = '10'
-    section['description'] = 'Capital'
-    section['parent'] = ' '
-    section = save_acc(section)
-    
+    section['parent'] = rout['name']
     sub_section = {}
-    sub_section['name'] = 104
-    sub_section['description'] = 'Capital of non - profit enterprises'
-    sub_section['acc_type'] = ACC_P
-    sub_section['parent'] = section['name']
-    sub_section = save_acc(sub_section)
-    # with open("notes.txt", "r") as fp:
-    #     for line in fp:
-    #         line = line[:-1]
-    #         sa = line.split(' | ')
-    #         print(sa)
-    #         print(len(sa))
+    
+    with open("notes.txt", "r") as fp:
+        for line in fp:
+            line = line[:-1]
+            sa = line.split(' | ')
+            if len(sa[0]) == 1:
+                section['name'] = sa[0]
+                section['description'] = sa[1]
+                section = save_acc(section)
+            elif len(sa[0]) == 2:
+                sub_section['name'] = sa[0]
+                sub_section['description'] = sa[1]
+                sub_section['parent'] = section['name']
+                sub_section = save_acc(sub_section)
+            else:
+                acc = {}
+                acc['name'] = sa[0]
+                acc['description'] = sa[1]
+                match sa[2]:
+                    case 'Active': acc['acc_type'] = ACC_A
+                    case 'Passive': acc['acc_type'] = ACC_P
+                    case _: acc['acc_type'] = ACC_AP
+                acc['parent'] = sub_section['name']
+                save_acc(acc)
     
     return Response(status=status.HTTP_201_CREATED)
     
