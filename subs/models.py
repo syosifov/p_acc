9000df5a447a2a0a9a707cb001952bc4ee51f014
Strashimir Yosifov <syosifov@gmail.com>
2022-07-18 17:08:35 +0300

    subs v2

--- subs/models.py
+++ subs/models.py
@@ -1,28 +1,37 @@
 from django.db import models
 from acc.models import Account, AccountHistory
 
+
 class Tax(models.Model):
-    name = models.CharField(max_length=30,unique=True)
-    # amount = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    name = models.CharField(max_length=30, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
 
 class AssignedTax(models.Model):
-    tax = models.ForeignKey(Tax,on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2,default=0)
-        
-        
+    tax = models.ForeignKey(Tax, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+
 class Subscriber(models.Model):
+
     name = models.CharField(max_length=20, null=False,
                             blank=False, unique=True, primary_key=True, default='0')
-    
-    account = models.OneToOneField(Account,on_delete=models.RESTRICT)
-    accountHistory = models.ForeignKey(AccountHistory, on_delete=models.DO_NOTHING)
+
+    account = models.OneToOneField(Account, on_delete=models.CASCADE) # TODO
+    accountHistory = models.ForeignKey(
+        AccountHistory, on_delete=models.DO_NOTHING)
 
     parent = models.ForeignKey('self',
                                default=None,
                                on_delete=models.PROTECT,
                                blank=True,
                                null=True)
+    
+    taxes = models.ManyToManyField(Tax)
+    assignedTaxes = models.ManyToManyField(AssignedTax)
 
     def __str__(self) -> str:
-        return self.name        
+        return self.name
+
+
