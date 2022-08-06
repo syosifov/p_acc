e93826a8cee5d9fd7d883289766a59c0d129a883
Strashimir Yosifov <syosifov@gmail.com>
2022-08-06 20:30:38 +0300

    v2

--- school/models.py
+++ school/models.py
@@ -1,5 +1,3 @@
-# https://github.com/PrettyPrinted/youtube_video_code/tree/master/2020/05/16/Adding%20Extra%20Fields%20On%20Many-To-Many%20Relationships%20in%20Django/many_to_many_extra
-
 from django.db import models
 
 
