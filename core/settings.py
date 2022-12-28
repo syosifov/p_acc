74e1651e846e53a9cf9cbf0958b8eef7f65adadf
Strashimir Yosifov <syosifov@gmail.com>
2022-12-28 13:31:59 +0200

    secret key and debug set.

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ne=kla7+won4-ala+p3y(+*+oau5br1k%3zacmgzyoeczrijr*'
+# SECRET_KEY = 'django-insecure-ne=kla7+won4-ala+p3y(+*+oau5br1k%3zacmgzyoeczrijr*'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 
+                            'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
