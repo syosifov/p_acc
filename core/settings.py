845880522ee330b48a647ba69df5c5c649c9ebe9
Strashimir Yosifov <syosifov@gmail.com>
2022-12-29 17:06:24 +0200

    ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pacc-production.up.railway.app',]
 
 
 # Application definition
